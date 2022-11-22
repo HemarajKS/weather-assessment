dd7a6a095e81ac15c40aae3fe0e89b8071b8989c
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-11-22 21:38:41 +0530

    first commit

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -38,7 +38,7 @@ const Header = () => {
         visibility: weather.data.data.current.vis_miles,
         fav: false,
       };
-
+    console.log(data);
     dispatch(currentSearch(data));
   }, [weather]);
 
--- src/redux/reducers/currentData.tsx
+++ src/redux/reducers/currentData.tsx
@@ -1,7 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  search: {},
+  search: {
+    id: '13.35,74.75',
+    place: 'Udupi',
+    region: 'Karnataka',
+    icon: '//cdn.weatherapi.com/weather/64x64/night/116.png',
+    temp_f: 73.6,
+    temp_c: 23.1,
+    condition: 'Partly cloudy',
+    temp_min: 71.6,
+    temp_max: 75.6,
+    precep: 0,
+    humidity: 73,
+    wind: 2.9,
+    visibility: 6,
+    fav: false,
+  },
 };
 
 export const searchSlice = createSlice({
