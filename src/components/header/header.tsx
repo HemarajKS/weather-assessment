0645efa842e81956285e4126cc29fc6eb7970a7d
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-11-22 12:46:10 +0530

    first commit

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import './header.css'
 
 const Header = () => {
-  return <div className="header">Header</div>
+  return (
+    <div className="header">
+      <div className="headerLogo">
+        <img src={require('../../assets/images/logo_web.png')} alt="Logo" />
+      </div>
+      <div className="headerSearch">
+        <input type="text" className="headerSearchInput" />
+      </div>
+    </div>
+  )
 }
 
 export default Header
