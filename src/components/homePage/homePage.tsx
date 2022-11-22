0c9935290c2fc8cdd2ade21c10b16e11b34257a6
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-11-22 12:38:18 +0530

    first commit

---
+++ src/components/homePage/homePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './homePage.css'
+
+const HomePage = () => {
+  return <div className="homePage">HomePage</div>
+}
+
+export default HomePage
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Footer from '../components/footer/footer'
+import Header from '../components/header/header'
+import HomePage from '../components/homePage/homePage'
+import './home.css'
 
 const Home = () => {
-  return <div className="home">Home</div>
+  return (
+    <div className="home">
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  )
 }
 
 export default Home
