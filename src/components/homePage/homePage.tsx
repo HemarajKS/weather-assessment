3b6f539572c1517c2be5fc34e1492213e9e07c27
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-11-22 19:57:36 +0530

    first commit

--- src/components/homePage/homePage.tsx
+++ src/components/homePage/homePage.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Footer from '../footer/footer'
-import './homePage.css'
+import React from 'react';
+import Footer from '../footer/footer';
+import './homePage.css';
+import { NavLink, Outlet } from 'react-router-dom';
+import Router from '../Router/Router';
 
 const HomePage = () => {
   return (
     <div className="homePage">
-      <div className="homePageBody">Body</div>
+      <div className="homePageBody">
+        <div className="homePageHeader"></div>
+        <div className="homePageBodyContents"></div>
+      </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/header/header';
-import HomePage from '../components/homePage/homePage';
+
+import Router from '../components/Router/Router';
 import './home.css';
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
-
-      <HomePage />
+      <Router />
     </div>
   );
 };
